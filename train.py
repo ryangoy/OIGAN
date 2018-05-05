bc16b67ecf83786eb101836f3bb9acbb9d4b3bd4
JEFFREY ZHANG <jozhang@berkeley.edu>
2018-05-05 14:06:38 -0700

    Restore, TB bug, Denormalize

--- train.py
+++ train.py
@@ -10,6 +10,8 @@ from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from networks import build_generator, build_discriminator, GANLoss, SLLoss, print_network
 
+from load_data import save_img
+
 import torch.backends.cudnn as cudnn
 
 from load_data import get_training_set, get_test_set
@@ -22,7 +24,7 @@ writer = SummaryWriter()
 # Training settings
 parser = argparse.ArgumentParser(description='OIGAN PyTorch implementation')
 parser.add_argument('--dataset', default='sunrgbd', help='type of dataset')
-parser.add_argument('--batchSize', type=int, default=1, help='training batch size')
+parser.add_argument('--batchSize', type=int, default=8, help='training batch size')
 parser.add_argument('--testBatchSize', type=int, default=1, help='testing batch size')
 parser.add_argument('--nEpochs', type=int, default=200, help='number of epochs to train for')
 parser.add_argument('--input_channels', type=int, default=8, help='input image channels')
@@ -42,6 +44,10 @@ parser.add_argument('--lamb', type=int, default=10, help='DEPRECIATED: weight on
 parser.add_argument('--l1_bonus', type=int, default=10, help='weight on L1 term in objective')
 parser.add_argument('--sl_bonus', type=int, default=1e9, help='weight on SLterm in objective')
 parser.add_argument('--gan_bonus', type=int, default=1, help='weight on gan term in objective')
+
+parser.add_argument('--restore_G', type=str, default=None, help='generative model file to use')
+parser.add_argument('--restore_D', type=str, default=None, help='discriminative model file to use')
+
 opt = parser.parse_args()
 
 print(opt)
@@ -62,21 +68,43 @@ test_set = get_test_set(root_path)
 training_data_loader = DataLoader(dataset=train_set, num_workers=opt.threads, batch_size=opt.batchSize, shuffle=True)
 testing_data_loader = DataLoader(dataset=test_set, num_workers=opt.threads, batch_size=opt.testBatchSize, shuffle=False)
 
+class UnNormalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        """
+        Args:
+            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+        Returns:
+            Tensor: Normalized image.
+        """
+        for t, m, s in zip(tensor, self.mean, self.std):
+            t.mul_(s).add_(m)
+            # The normalize code -> t.sub_(m).div_(s)
+        return tensor
+
+unorm = UnNormalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
 
 def add_images(imgs, writer, name, num_steps):
     imgs = imgs.clone()
     _, C, _, _ = imgs.shape
     img = imgs[0].cpu()
     if C == 3:
+        img = unorm(img)
         writer.add_image(name, img, num_steps)
 
     if C == 4:
         rgb, d = torch.split(img, [3,1], dim=0)
+        rgb = unorm(rgb)
         writer.add_image(name + "RGB", rgb, num_steps)
         writer.add_image(name + "D", d, num_steps)
 
     if C == 8:
-        fore_rgb, fore_d, back_rgb, back_d = np.array_split(img,  [3,4,7],axis=0)
+        back_rgb, back_d, fore_rgb, fore_d = np.array_split(img,  [3,4,7],axis=0)
+        fore_rgb = unorm(fore_rgb)
+        back_rgb = unorm(back_rgb)
         writer.add_image(name + "foreground RGB", fore_rgb, num_steps)
         writer.add_image(name + "foreground D", fore_d, num_steps)
         writer.add_image(name + "background RGB", back_rgb, num_steps)
@@ -90,7 +118,7 @@ def train(epoch):
         # forward
         real_a_cpu, real_b_cpu, coords_cpu = batch[0], batch[1], batch[2]
 
-        if iteration == 0:
+        if iteration == 1:
             add_images(real_a_cpu, writer, "Input Image A", epoch)
             add_images(real_b_cpu, writer, "Input Image B", epoch)
 
@@ -99,7 +127,7 @@ def train(epoch):
         coords.data.resize_(coords_cpu.size()).copy_(coords_cpu)
 
         fake_b = G(real_a)
-        if iteration == 0:
+        if iteration == 1:
             add_images(fake_b, writer, "Fake Image", epoch)
 
         ############################
@@ -112,7 +140,7 @@ def train(epoch):
         fake_ab = torch.cat((real_a, fake_b), 1)
         pred_fake = D.forward(fake_ab.detach())
         loss_d_fake = criterionGAN(pred_fake, False)
-        if iteration == 0:
+        if iteration == 1:
             writer.add_scalar("loss_discriminator_fake", loss_d_fake, epoch) 
 
 
@@ -121,13 +149,13 @@ def train(epoch):
         pred_real = D.forward(real_ab)
         #writer.add_scalar("discriminator_entropy", entropy(pred_real), epoch)   TODO
         loss_d_real = criterionGAN(pred_real, True)
-        if iteration == 0:
+        if iteration == 1:
             writer.add_scalar("loss_discriminator_real", loss_d_real, epoch) 
 
         
         # Combined loss
         loss_d = (loss_d_fake + loss_d_real) * 0.5
-        if iteration == 0:
+        if iteration == 1:
             writer.add_scalar("loss_discriminator", loss_d, epoch) 
 
         loss_d.backward()
@@ -142,19 +170,19 @@ def train(epoch):
         fake_ab = torch.cat((real_a, fake_b), 1)
         pred_fake = D.forward(fake_ab)
         loss_g_gan = criterionGAN(pred_fake, True) * opt.gan_bonus
-        if iteration == 0:
+        if iteration == 1:
             writer.add_scalar("loss_generator_gan", loss_g_gan, epoch) 
 
          # Second, G(A) = B
         loss_g_l1 = criterionL1(fake_b, real_b) * opt.l1_bonus
-        if iteration == 0:
+        if iteration == 1:
             writer.add_scalar("loss_generator_l1", loss_g_l1, epoch) 
         loss_g_sl = criterionSLL(fake_b, real_b, coords) * opt.sl_bonus
-        if iteration == 0:
+        if iteration == 1:
             writer.add_scalar("loss_generator_sl", loss_g_sl, epoch) 
         
         loss_g = loss_g_gan + loss_g_l1 + loss_g_sl
-        if iteration == 0:
+        if iteration == 1:
             writer.add_scalar("loss_generator", loss_g, epoch) 
         
         loss_g.backward()
@@ -166,7 +194,7 @@ def train(epoch):
 
 def validate(epoch):
     avg_psnr = 0
-    for iteration, batch in enumerate(testing_data_loader):
+    for iteration, batch in enumerate(testing_data_loader, 1):
         input, target = Variable(batch[0], volatile=True), Variable(batch[1], volatile=True)
         if opt.cuda:
             input = input.cuda()
@@ -176,7 +204,7 @@ def validate(epoch):
         mse = criterionMSE(prediction, target)
         psnr = 10 * log10(1 / mse.data[0])
         avg_psnr += psnr
-        if iteration == 0:
+        if iteration == 1:
             add_images(prediction, writer, "Fake Image (Validation)", epoch)
     writer.add_scalar("PSNR (Validation)", avg_psnr, epoch)
     print("===> Avg. PSNR: {:.4f} dB".format(avg_psnr / len(testing_data_loader)))
@@ -197,10 +225,16 @@ def checkpoint(epoch):
 
 
 # Make the model
-G = build_generator(opt.input_channels, opt.output_channels, opt.num_gen_filters, opt.num_gen_ds, opt.num_gen_blocks, 
+if opt.restore_G is None:
+    G = build_generator(opt.input_channels, opt.output_channels, opt.num_gen_filters, opt.num_gen_ds, opt.num_gen_blocks, 
                     norm_fn=nn.BatchNorm2d, use_dropout=False, gpu_ids=[0])
+else:
+    G = torch.load(opt.restore_G)
 
-D = build_discriminator(opt.input_channels + opt.output_channels, opt.num_dis_filters, norm_fn=nn.BatchNorm2d, use_sigmoid=False, gpu_ids=[0])
+if opt.restore_D is None:
+    D = build_discriminator(opt.input_channels + opt.output_channels, opt.num_dis_filters, norm_fn=nn.BatchNorm2d, use_sigmoid=False, gpu_ids=[0])
+else:
+    D = torch.load(opt.restore_D)
 
 
 # Set up loss
